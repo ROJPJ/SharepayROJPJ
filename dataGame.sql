a216006e43431fd88fe07bac34ce639a7dd33452
Boes Julian <julian.boes@decathlon.com>
2018-05-14 18:05:12 +0200

    first sql sharepay

---
+++ dataGame.sql
@@ -0,0 +1,20 @@
+INSERT INTO "public"."user" ("id", "name") VALUES ('059bd697-3d1b-4c61-b9fa-76a249bd519e', 'Julian'),
+('3b21268e-8e00-4ca3-91ea-0cac02bd4cf0', 'Olivier'),
+('661a1ebe-ad31-4c35-8520-af24e26c8a57', 'Remi'),
+('e49ded9b-b08d-4e9e-a8ba-6b77504dc9d9', 'Jean Philippe');
+
+INSERT INTO "public"."event" ("id", "label", "description", "date", "currency", "status_id", "user_id") VALUES ('440e057d-f0ab-4da6-bb64-00b55895cd31', 'Dégustation Lille', 'Bières', '2018-05-14', NULL, 'A', 'e49ded9b-b08d-4e9e-a8ba-6b77504dc9d9'),
+('c08f74b1-5246-4b76-a153-c7f0a4e1f2e2', 'TMB', 'Tour du Mt Blanc', '2018-05-14', NULL, 'A', 'e49ded9b-b08d-4e9e-a8ba-6b77504dc9d9');
+
+
+INSERT INTO "public"."event_user" ("event_id", "user_id") VALUES ('440e057d-f0ab-4da6-bb64-00b55895cd31', '661a1ebe-ad31-4c35-8520-af24e26c8a57'),
+('440e057d-f0ab-4da6-bb64-00b55895cd31', 'e49ded9b-b08d-4e9e-a8ba-6b77504dc9d9'),
+('c08f74b1-5246-4b76-a153-c7f0a4e1f2e2', '3b21268e-8e00-4ca3-91ea-0cac02bd4cf0');
+
+
+INSERT INTO "public"."expense" ("id", "label", "event_id", "amount", "date", "user_id") VALUES ('a07fc862-82af-4dad-b275-74eeacd5c0e3', 'Taxi pour rentrer', '440e057d-f0ab-4da6-bb64-00b55895cd31', NULL, NULL, 'e49ded9b-b08d-4e9e-a8ba-6b77504dc9d9'),
+('c72c4df5-6b7e-4d8f-9e12-e9b3e9a97be1', 'Refuge Elisabetta', 'c08f74b1-5246-4b76-a153-c7f0a4e1f2e2', '5000', '2018-05-14', '3b21268e-8e00-4ca3-91ea-0cac02bd4cf0');
+
+
+INSERT INTO "public"."expense_user" ("expense_id", "user_id") VALUES ('a07fc862-82af-4dad-b275-74eeacd5c0e3', 'e49ded9b-b08d-4e9e-a8ba-6b77504dc9d9'),
+('c72c4df5-6b7e-4d8f-9e12-e9b3e9a97be1', '3b21268e-8e00-4ca3-91ea-0cac02bd4cf0');
---
+++ tablePlusSharepay.sql
@@ -0,0 +1,47 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE "public"."user" (
+   "id" uuid DEFAULT gen_random_uuid() NOT NULL,
+   "name" varchar,
+   PRIMARY KEY ("id")
+);
+
+CREATE TABLE "event" (
+   "id" uuid DEFAULT gen_random_uuid() NOT NULL,
+   "label" varchar,
+   "description" text,
+   "date" date,
+   "currency" varchar,
+   "status_id" bpchar(1),
+   "user_id" uuid not null,
+   CONSTRAINT "event_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id"),
+   PRIMARY KEY ("id")
+);
+
+CREATE TABLE "event_user" (
+   "event_id" uuid,
+   "user_id" uuid not null,
+   CONSTRAINT "event_user_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "event"("id"),
+   CONSTRAINT "event_user_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id"),
+   PRIMARY KEY ("event_id","user_id")
+);
+
+CREATE TABLE "expense" (
+   "id" uuid DEFAULT gen_random_uuid() NOT NULL,
+   "label" varchar,
+   "event_id" uuid not null,
+   "amount" int4,
+   "date" date,
+   "user_id" uuid not null,
+   CONSTRAINT "expense_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "event"("id"),
+   CONSTRAINT "expense_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id"),
+   PRIMARY KEY ("id")
+);
+
+CREATE TABLE "public"."expense_user" (
+   "expense_id" uuid,
+   "user_id" uuid,
+   CONSTRAINT "expense_user_expense_id_fkey" FOREIGN KEY ("expense_id") REFERENCES "public"."expense"("id"),
+   CONSTRAINT "expense_user_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id"),
+   PRIMARY KEY ("expense_id","user_id")
+);
